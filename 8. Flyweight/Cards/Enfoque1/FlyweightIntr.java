95958ffa2729c29b95070bd8cf7c00286738a0ce
isa9906 <isa8.perez@hotmail.com>
2019-10-16 08:54:17 -0500

    Patron flyweight

--- 8. Flyweight/Cards/Enfoque1/FlyweightIntr.java
+++
@@ -1,7 +0,0 @@
-public interface FlyweightIntr {
-  public String getCompany();
-  public String getAddress();
-  public String getCity();
-  public String getState();
-  public String getZip();
-}
--- 8. Flyweight/Cards/Enfoque2/FlyweightIntr.java
+++
@@ -1,8 +0,0 @@
-public interface FlyweightIntr {
-  public String getCompany();
-  public String getAddress();
-  public String getCity();
-  public String getState();
-  public String getZip();
-  public void print(String name, String title);
-}
